e8638a02807379f126e2bde06eb91baef33aa506
Rajithxx <148357145+Rajithxx@users.noreply.github.com>
2024-04-20 12:46:58 +0530

    212221060222 HALF_ADDER.v

--- HALF_ADDER.srcs/sources_1/new/HALF_ADDER.v
+++ HALF_ADDER.srcs/sources_1/new/HALF_ADDER.v
@@ -1,29 +1,6 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.01.2024 14:30:14
-// Design Name: 
-// Module Name: HALF_ADDER
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module half_adder(a,b,sum,carry);
-input a,b;
-output sum,carry; // Write the logic for sum and carry in gatelevel modeling
-
-
+module ha(a,b,sum,carry);
+input a,b;  
+output sum,carry;    
+xor g1(sum,a, b);
+and g2(carry, a, b);
 endmodule
-
